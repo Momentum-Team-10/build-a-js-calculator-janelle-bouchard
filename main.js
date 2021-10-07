1171517a6b6e9a3ad89ca50e3d636f491a21147d
janelle-bouchard <91432686+janelle-bouchard@users.noreply.github.com>
2021-10-07 17:39:59 -0400

    1st draft, functionality complete, playing with styling

--- main.js
+++ main.js
@@ -1,40 +1,64 @@
-let display = document.getElementById(`display`)
-let b1 = document.getElementByID(`1`)
-let b2 = document.getElementByID(`2`)
-let b3 = document.getElementByID(`3`)
-let b4 = document.getElementByID(`4`)
-let b5 = document.getElementByID(`5`)
-let b6 = document.getElementByID(`6`)
-let b7 = document.getElementByID(`7`)
-let b8 = document.getElementByID(`8`)
-let b9 = document.getElementByID(`9`)
-let b0 = document.getElementByID(`0`)
+let display = document.getElementById("display")
+// let b1 = document.getElementByID("b1")
+// let b2 = document.getElementByID("b2")
+// let b3 = document.getElementByID("b3")
+// let b4 = document.getElementByID("b4")
+// let b5 = document.getElementByID("b5")
+// let b6 = document.getElementByID("b6")
+// let b7 = document.getElementByID("b7")
+// let b8 = document.getElementByID("b8")
+// let b9 = document.getElementByID("b9")
+// let b0 = document.getElementByID("b0")
+// let divide = document.getElementByID("bdivide")
+// let add = document.getElementByID("badd")
+// let subtract = document.getElementByID("bsubtract")
+// let decimal = document.getElementByID("bdecimal")
 
-// b1.addEventListener(`click`, () => {
+// b1.addEventListener("click", () => {
 //     display.innerText += b1.innerText
 // })
 
-let buttons = document.querySelectorAll(`.button`)
-console.log(buttons)
-// This brings all values in as an array!
+
+let buttons = document.querySelectorAll(".buttons")
+
 for (let button of buttons) {
-    button.addEventListener (`click`, () => {
+    button.addEventListener ("click", () => {
         display.innerText += button.innerText
     })
 }
 
-let equals = document.getElementById(`equals`)
+let equals = document.getElementById("equals")
 
-equals.addEventListener(`click`, () => {
+equals.addEventListener ("click", () => {
     display.innerText = eval(display.innerText)
+})
+
+let clear = document.getElementById("bclear")
+
+clear.addEventListener ("click", () => {
+    display.innerText = (display.innerText = "")
 })
 
 
 
 
+// display.addEventListener ("input", () => {
+// })
+
+// let multiply = document.getElementByID("bmultiply")
+// multiply.operation = "*"
+
+// multiply.addEventListener ("click", () => {
+// display.replace("x", "*")
+// })
+
+// let multiply = document.getElementByID("bmultiply")
+// let multiply.operation = *
 
 
 
+
+// Notes from Class:
 // let elementToRemove = document.querySelectorAll("li")[2]
 // elementToRemove.remove()
 
@@ -48,12 +72,12 @@ equals.addEventListener(`click`, () => {
 // newElement.innerText = 'Hello World!'
 
 // using .innerText is how we can make the number and symbols show on the calc screen
-// Template literals: You HAVE to use `` instead of quotes
+// Template literals: You HAVE to use "" instead of quotes
 // let name = "Mom"
-// let content = `<h2>Hello $(name)</h2>`;
+// let content = "<h2>Hello $(name)</h2>";
 
 
-// newElement.innerText = `${personName}`
+// newElement.innerText = "${personName}"
 // root.appendChild(newElement)
 
 // // Instead of 
@@ -66,3 +90,7 @@ equals.addEventListener(`click`, () => {
 // }
 // let personName = object.name
 // newElement.innerText = 
+
+// let buttons = document.querySelectorAll(".buttons")
+// This brings all values in as an array!
+// console.log (buttons)
